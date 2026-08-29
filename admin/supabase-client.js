d05d5ccc4000dca13d56dc367dda1eede9fc293d
aalameenalsir-sudo <aalameenalsir@gmail.com>
2026-08-29 23:14:00 +0300

    Rename supabase variable to supabaseClient

--- admin/supabase-client.js
+++ admin/supabase-client.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://bgxtcpcbkjftkgswpizr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_t9MKvlsLheyCj-o-jIQ04A_9sGI_j_r";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
